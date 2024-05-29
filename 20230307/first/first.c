f87d7d09c389d2dcee0c2484b3a05d0f276fee67
Ramee <143769096+LsRamee@users.noreply.github.com>
2024-05-29 10:34:59 +0900

    Delete 20230307 directory

--- 20230307/first/first.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    printf("영남이공대학교 사이버보안스쿨입니다.\n");
-    return 0;
-}
--- 20230307/third/third.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-	scanf();
-	printf("%s´Â ¾Æ´Õ´Ï´Ù.\n");
-}
